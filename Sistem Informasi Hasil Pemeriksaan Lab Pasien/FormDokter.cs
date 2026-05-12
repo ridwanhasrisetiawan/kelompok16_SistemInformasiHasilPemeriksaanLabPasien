7158f2d45f8b4c4f22854760e476222e37b09572
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-05-12 15:41:01 +0700

    membuat button insert pada form dokter

--- Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.cs
+++ Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.cs
@@ -39,7 +39,49 @@ namespace Sistem_Informasi_Hasil_Pemeriksaan_Lab_Pasien
 
         private void btnInsert_Click(object sender, EventArgs e)
         {
+            try
+            {
+                conn.Open();
 
+                SqlCommand cmd = new SqlCommand(
+                    "sp_InsertDokter", conn);
+
+                cmd.CommandType =
+                    CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue(
+                    "@nama",
+                    txtNamaDokter.Text);
+
+                cmd.Parameters.AddWithValue(
+                    "@spesialis",
+                    txtSpesialis.Text);
+
+                cmd.Parameters.AddWithValue(
+                    "@email",
+                    txtEmail.Text);
+
+                cmd.Parameters.AddWithValue(
+                    "@password",
+                    txtPassword.Text);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show(
+                    "Data Dokter Berhasil Ditambah");
+
+                conn.Close();
+
+                TampilData();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error : " + ex.Message);
+
+                conn.Close();
+            }
         }
     }
 }
