d143b4e077493043e7cdb262e00e65892426a9db
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-05-12 15:43:14 +0700

    membuat button delete pada form dokter

--- Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.Designer.cs
+++ Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.Designer.cs
@@ -304,6 +304,7 @@
             this.btnDelete.TabIndex = 15;
             this.btnDelete.Text = "DELETE";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnView
             // 
--- Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.cs
+++ Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormDokter.cs
@@ -135,5 +135,41 @@ namespace Sistem_Informasi_Hasil_Pemeriksaan_Lab_Pasien
                 conn.Close();
             }
         }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+
+                SqlCommand cmd =
+                    new SqlCommand(
+                    "sp_DeleteDokter", conn);
+
+                cmd.CommandType =
+                    CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue(
+                    "@id",
+                    txtId.Text);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show(
+                    "Data Berhasil Dihapus");
+
+                conn.Close();
+
+                TampilData();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Error : " + ex.Message);
+
+                conn.Close();
+            }
+        }
     }
 }
