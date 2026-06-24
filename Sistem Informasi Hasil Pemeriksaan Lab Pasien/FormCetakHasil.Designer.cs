efecd96de136fbd37693f80a9cc4a1acc30db3b8
ridwanhasrisetiawan <ridwanhasrisetiawan@gmail.com>
2026-06-24 17:30:04 +0700

    membuat cetak hasil

---
+++ Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormCetakHasil.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Sistem_Informasi_Hasil_Pemeriksaan_Lab_Pasien
+{
+    partial class FormCetakHasil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.CrystalReport11 = new Sistem_Informasi_Hasil_Pemeriksaan_Lab_Pasien.CrystalReport1();
+            this.SuspendLayout();
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = 0;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ReportSource = this.CrystalReport11;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
+            this.crystalReportViewer1.TabIndex = 0;
+            // 
+            // FormCetakHasil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Name = "FormCetakHasil";
+            this.Text = "FormCetakHasil";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private CrystalReport1 CrystalReport11;
+    }
+}
---
+++ Sistem Informasi Hasil Pemeriksaan Lab Pasien/FormCetakHasil.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Sistem_Informasi_Hasil_Pemeriksaan_Lab_Pasien
+{
+    public partial class FormCetakHasil : Form
+    {
+        SqlConnection conn =
+            new SqlConnection(
+            "Data Source=LAPTOP-4VAVDOFH\\WAWANLOMBOK;Initial Catalog=HasilPemeriksaanLabDB;Integrated Security=True");
+
+        int idPasien;
+
+        public FormCetakHasil(int id)
+        {
+            InitializeComponent();
+
+            idPasien = id;
+
+            LoadReport();
+        }
+
+        private void LoadReport()
+        {
+            try
+            {
+                SqlCommand cmd =
+                    new SqlCommand(
+                        "sp_ReportHasilLab",
+                        conn);
+
+                cmd.CommandType =
+                    CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue(
+                    "@id_pasien",
+                    idPasien);
+
+                SqlDataAdapter da =
+                    new SqlDataAdapter(cmd);
+
+                DataTable dt =
+                    new DataTable();
+
+                da.Fill(dt);
+
+                CrystalReport1 rpt =
+                    new CrystalReport1();
+
+                rpt.SetDataSource(dt);
+
+                crystalReportViewer1.ReportSource =
+                    rpt;
+
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    ex.Message);
+            }
+        }
+    }
+}
